ceab4032c5c087b8af42b0d9cd02cfa057ee19fb
yitzamm <sidsyh@gmail.com>
2026-02-23 14:00:11 -0600

    Fix security scan issues

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app.main import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+    app.run(host="0.0.0.0", port=5000, debug=debug_mode)

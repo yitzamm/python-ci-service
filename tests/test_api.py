c3c5fdda46052e755728d31bfb3784c4ce4e8543
yitzamm <sidsyh@gmail.com>
2026-02-23 14:14:26 -0600

    Fix test file formatting for Black, Flake8 and Bandit

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,6 +7,6 @@ def test_get_vegetables():
 
     response = client.get("/vegetables")
 
-    assert response.status_code == 200  #  nosec: safe test code
-    assert response.json["count"] == 5  #  nosec: safe test code
-    assert len(response.json["data"]) == 5  #  nosec: safe test code
+    assert response.status_code == 200  # nosec: safe test code
+    assert response.json["count"] == 5  # nosec: safe test code
+    assert len(response.json["data"]) == 5  # nosec: safe test code

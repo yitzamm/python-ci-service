a9f207ff60818f58297223a6fa72020c1e51bade
yitzamm <sidsyh@gmail.com>
2026-02-23 03:41:23 -0600

    New multiply function

--- app/main.py
+++ app/main.py
@@ -1,2 +1,5 @@
 def add(a, b):
-    return a - b
+    return a + b
+
+def multiply(a, b):
+    return a * b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,14 @@
 from app.main import add
+from app.main import multiply
 
 def test_add_success():
     assert add(2, 3) == 5
 
 def test_add_negative():
     assert add(-1, 1) == 0
+
+def test_multiply_success():
+    assert multiply(2, 3) == 6
+
+def test_multiply_negative():
+    assert multiply(-1, 1) == -1
